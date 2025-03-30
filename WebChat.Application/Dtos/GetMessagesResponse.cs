b6f183a8324fc2c32ed8e32f785304fc16d9a590
Artsiukh Daniil <artjx_da_22@mf.grsu.by>
2025-03-30 14:40:13 +0300

    ChatHub + deleted old dto

--- WebChat.Application/Dtos/GetMessagesResponse.cs
+++
@@ -1,7 +0,0 @@
-public record GetMessagesResponse(
-    int Id,
-    string? content,
-    DateTime CreatedDate,
-    GetUserResponse Sender,
-    int ChatId
-);
--- WebChat.Infrastructure/Hub/ChatHub.cs
+++ WebChat.Infrastructure/Hub/ChatHub.cs
@@ -1,9 +1,70 @@
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
-public class ChatHub(DatabaseContext context) : Hub {
-    public async Task SendMessage(string user, string message)
+[Authorize]
+public class ChatHub : Hub {
+    public IRepository<Message> MessageRepository {get;set;}
+    public IMessageService MessageService {get;set;}
+
+    public ChatHub(IRepository<Message> messageRepository, IMessageService messageService){
+        MessageRepository = messageRepository;
+        MessageService = messageService;
+    }
+    
+
+    public async Task JoinChat(int chatId){
+        var groupName = GetGroupName(chatId);
+        await Groups.AddToGroupAsync(Context.ConnectionId,groupName);
+        await Clients.Group(groupName).SendAsync("ReceiveMessage", "LOL");    
+    }
+
+    public async Task SendMessage(int chatId, string content){
+        var groupName = GetGroupName(chatId);
+        var userId = GetUserId();
+        var userName = GetUsername();
+
+        var createMessage = new CreateMessage(
+            content,
+            userId,
+            chatId
+        );
+
+        var message = await MessageService.CreateMessage(createMessage); 
+
+        var messageToSent = new GetMessage(
+            message.Id,
+            message.Content,
+            message.CreatedDate,
+            new GetUserResponse(
+                userId,
+                userName
+            )
+        );
+
+        await Clients.Group(groupName).SendAsync("ReceiveMessage", messageToSent);
+    }
+
+    public string GetGroupName(int chatId){
+
+        return $"chat_{chatId}";
+    }
+
+    private int GetUserId()
     {
-        await Clients.All.SendAsync("ReceiveMessage", user, message);
+        return int.Parse(
+            GetUserClaims().FindFirstValue("Id") ?? throw new NullReferenceException(" userId null reference")
+            );
+    }
+
+    private string GetUsername()
+    {
+        return GetUserClaims().FindFirstValue("Username") ?? throw new NullReferenceException(" username null reference");
+    }
+
+    private ClaimsPrincipal GetUserClaims()
+    {
+        return Context.User ?? throw new HubException("User not authenticated");
     }
 }
 
